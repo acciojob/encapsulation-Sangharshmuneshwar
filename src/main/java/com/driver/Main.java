2b4aad2732a4789c719bb1c9f6be4b38a3623837
Sangharsh Muneshwar <smuneshwar164@gmail.com>
2023-08-12 20:18:00 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+    RWOnly r1 = new RWOnly();
+//    r1.name = "Sangharsh";
+//    System.out.println(r1.name);
+
+    //'name' has private access in 'com.driver.RWOnly'
+    r1.setName("vaibhav");
+    r1.getName();
+  }
 }
